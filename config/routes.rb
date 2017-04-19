ef2e355b3fa127f6529b09dcdfbe46d7935239f3
David Capella <D_Capella@yahoo.com>
2017-04-19 09:33:59 -0500

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/home'
+  resources :posts
 
-  get 'welcome/index'
+  get 'about' => 'welcome#about'
 
-  get 'welcome/about'
-
-  root 'welcome#home'
+  root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
